106fa54e7093435cdae4951006b412c261d09f6a
Neil <ebox381@gmail.com>
2026-06-11 00:37:40 -0400

    finished group anagrams in python, keep thing to understand syntactically is that hashmaps in python can take any data structure as a value; in this case a list

---
+++ problems/groupAnagrams.py
@@ -0,0 +1,27 @@
+# Key py trick to remember is that a hashmap(dict) can store list as values
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        words = {}
+        for word in strs:
+            sorted_word = "".join(sorted(word))
+
+            if sorted_word not in words:
+                words[sorted_word] = []
+
+            words[sorted_word].append(word)
+    
+        final_list = []
+        for key in words:
+            final_list.append(words[key])
+
+        # Could also just do return list(words.values())
+
+        return final_list
+
+
+
+             
+
+
+
