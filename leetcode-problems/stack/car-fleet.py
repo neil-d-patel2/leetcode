864dcfc5a32947db15b86266e6fb0e81a75a6cd8
Neil <ebox381@gmail.com>
2026-06-19 10:36:29 -0400

    done

---
+++ leetcode-problems/stack/car-fleet.py
@@ -0,0 +1,73 @@
+'''
+
+Car Fleet
+Medium
+Topics
+Company Tags
+Hints
+There are n cars traveling to the same destination on a one-lane highway.
+
+You are given two arrays of integers position and speed, both of length n.
+
+position[i] is the position of the ith car (in miles)
+speed[i] is the speed of the ith car (in miles per hour)
+The destination is at position target miles.
+
+A car can not pass another car ahead of it. It can only catch up to another car and then drive at the same speed as the car ahead of it.
+
+A car fleet is a non-empty set of cars driving at the same position and same speed. A single car is also considered a car fleet.
+
+If a car catches up to a car fleet the moment the fleet reaches the destination, then the car is considered to be part of the fleet.
+
+Return the number of different car fleets that will arrive at the destination.
+
+Example 1:
+
+Input: target = 10, position = [1,4], speed = [3,2]
+
+Output: 1
+Explanation: The cars starting at 1 (speed 3) and 4 (speed 2) become a fleet, meeting each other at 10, the destination.
+
+Example 2:
+
+Input: target = 10, position = [4,1,0,7], speed = [2,2,1,1]
+
+Output: 3
+Explanation: The cars starting at 4 and 7 become a fleet at position 10. The cars starting at 1 and 0 never catch up to the car ahead of them. Thus, there are 3 car fleets that will arrive at the destination.
+
+Constraints:
+
+n == position.length == speed.length.
+1 <= n <= 1000
+0 < target <= 1000
+0 < speed[i] <= 100
+0 <= position[i] < target
+All the values of position are unique.
+
+
+Notes:
+
+It looks like the last car is essentially the car we need to use as an example. if an x car in the set of cars can reach thelast car <= the time it takes for the last car to reach the final destination, then we have a car in the set of car fleet. 
+'''
+
+
+
+
+
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+
+        # 3 
+        pair = [(p, s) for p, s in zip(position, speed)]
+        pair.sort(reverse=True)
+
+        fleets = 1 
+        prevTime = (target - pair[0][0]) / pair[0][1]
+        
+        for i in range(1, len(pair)):
+            currCar = pair[i]
+            currTime = (target - currCar[0]) / currCar[1]
+            if currTime > prevTime:
+                fleets += 1
+                prevTime = currTime
+        return fleets
